ad3feab824fe040502504ea0f9181f84657243c7
akai <lorenc@akai.com>
2019-01-17 13:11:25 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,28 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});s
+// });
+
+var hashedPassword =
+	'$2a$10$B74Rn/ektnq0KxgfOqxZuOWwc.ZKReErgCl1xEOvPLjE1o7LbsegS';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 // jwt.verify
 
 // var message = 'I am user numer 3';
